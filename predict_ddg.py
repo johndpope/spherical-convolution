eed1ffc1ee50fc76f29c68df712eeb37268c03b4
Thorn Hjalte Thorn Jakobsen <thornjakobsen@gmail.com>
2017-12-02 13:41:58 +0100

    changes to ddg predictor

--- predict_ddg.py
+++ predict_ddg.py
@@ -3,7 +3,9 @@ sys.path.append("../deepfold/") # append path to deepfold library
 from deepfold_predict_ddg import predict_ddg, read_ddg_csv, MissingResidueError
 from CNN import CNNCubedSphereModel
 import scipy.stats
+import pickle
 import argparse
+import os
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--markov-model-filename", dest="markov_model_filename",
@@ -34,9 +36,16 @@ for mutation in mutations:
     except MissingResidueError as e:
         sys.stderr.write("SKIPPING DUE TO MissingResidueError: "+ e + "\n")
 
-    ddg_list[0] += pred_ddg
-    ddg_list[1] += ddg
+    for i, dd in enumerate(pred_ddg):
+        ddg_list[0].append(pred_ddg[i])
+        ddg_list[1].append(ddg[i])
 
-    print(ddg_list)
+    print(*ddg_list)
+
+path = 'data/'
+if not os.path.exists(os.path.dirname(path)):
+    os.makedirs(os.path.dirname(path))
+with open(os.path.join(path, 'ddg_and_probs.p'), 'wb') as f:
+    pickle.dump(ddg_list, f)
 pcorr, pvalue  = scipy.stats.pearsonr(*ddg_list)
 print("correlation: ", pcorr)
